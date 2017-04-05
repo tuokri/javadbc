298c0bd46104cc9139c3c32799188a9608ab7956
Tuomo Kriikkula <tuomo.kriikkula@student.oulu.fi>
2017-04-05 20:42:03 +0300

    config file I/O

--- src/jdbcconnectivity/JDBCTunnel.java
+++ src/jdbcconnectivity/JDBCTunnel.java
@@ -1,5 +1,25 @@
 package jdbcconnectivity;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class JDBCTunnel {
-	
+
+    public static void main(String[] args) {
+
+        try(BufferedReader dbFile = new BufferedReader(new FileReader("config.properties"))) {
+
+
+        } catch(IOException ioe) {
+
+            System.out.println("IOException!");
+            ioe.printStackTrace();
+            System.exit(0);
+
+        }
+
+
+    }
+
 }
