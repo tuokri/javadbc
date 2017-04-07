23303ef3f9b0cdda133a00ca4b5528a028c7003b
Tuomo Kriikkula <tuomo.kriikkula@student.oulu.fi>
2017-04-07 23:27:06 +0300

    major cleanup of code

--- src/jdbcconnectivity/JDBCResponder.java
+++ src/jdbcconnectivity/JDBCResponder.java
@@ -1,29 +1,12 @@
-import java.io.DataInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.ByteBuffer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-// TODO: when ran via CLI, connect to database, open inSocket and outSocket, wait for start-message, send greetings to home, wait for response ... do stuff
-
 public class JDBCResponder {
 
-    private static final int portMin = 5000;
-    private static final int portMax = 15000;
-
     public static void main(String[] args) {
-
-        int portAmount = portMax - portMin + 1;
-        int[] ports = new int[portAmount];
-        for(int i = 0; i < portAmount; i++) {
-
-            ports[i] = portMin + i;
-
-        }
 
         String query = null;
         String dbConnStr = null;
@@ -36,19 +19,20 @@ public class JDBCResponder {
 
         } catch(ClassNotFoundException e) {
 
-            System.out.println("JDBCResponder: Oracle JDBC Driver not found!");
+            System.out.println("JDBCResponder: Oracle JDBC Driver not found.");
             e.printStackTrace();
             System.exit(0);
 
         }
 
-        System.out.println("JDBCResponder: Oracle JDBC Driver registered!");
+        System.out.println("JDBCResponder: Oracle JDBC Driver registered.");
 
         try {
 
             dbConnStr = args[0];
             dbUname = args[1];
             dbPassw = args[2];
+            query = args[3];
 
         } catch(Exception e) {
 
@@ -57,92 +41,39 @@ public class JDBCResponder {
 
         }
 
-        ServerSocket server = null;
-        Socket client = null;
         ResultSet rs = null;
+        String rsString = null;
         try {
 
             Connection connection = DriverManager.getConnection(dbConnStr, dbUname, dbPassw);
-            System.out.println("JDBCResponder: DBCONNOK");
-
-            for (int port : ports) {
-
-                try {
-
-                    server = new ServerSocket(port);
-                    System.out.println("JDBCResponder: PORT " + port);
-                    break;
-
-                } catch(Exception e) {
-
-                    // Port was closed.
-
-                }
-            }
-
-            byte[] messageByte = new byte[1000];
-            boolean end = false;
-            String messageString = "";
-
-            client = server.accept();
-
-            DataInputStream clientIn = new DataInputStream(client.getInputStream());
-            int bytesRead = 0;
-
-            messageByte[0] = clientIn.readByte();
-            messageByte[1] = clientIn.readByte();
-            ByteBuffer byteBuffer = ByteBuffer.wrap(messageByte, 0, 2);
-
-            int bytesToRead = byteBuffer.getShort();
-
-            while(!end) {
-
-                bytesRead = clientIn.read(messageByte);
-                messageString += new String(messageByte, 0, bytesRead);
-                if(messageString.length() == bytesToRead) {
-
-                    end = true;
-
-                }
-            }
+            System.out.println("JDBCResponder: Connected to database.");
 
             try {
 
-                query = "SELECT * FROM YEAR";
                 Statement stmt = connection.createStatement();
-                System.out.println("QUERY : " + query);
-                rs = stmt.executeQuery(query);
 
+                rs = stmt.executeQuery(query);
+                System.out.println("JDBCResponder: Executing query...");
                 while(rs.next()) {
 
-                    System.out.println(rs.getString("YEAR"));
+                    rsString += rs.getString(1);
 
                 }
 
+                System.out.println(rsString);
+
             } catch(SQLException e) {
 
-                System.out.println("JDBCResponder: SQL error!");
-                System.out.println(e.toString());
+                System.out.println("JDBCResponder: SQL error.");
+                System.out.println(e);
 
             }
 
         } catch(Exception e) {
 
-            System.out.println("JDBCResponder: Error!");
-            System.out.println(e.toString());
+            System.out.println("JDBCResponder: Error.");
+            System.out.println(e);
 
-        } finally {
-
-            try {
-
-                if(server != null) server.close();
-                if(client != null) client.close();
-
-            } catch(Exception e) {
-
-                e.printStackTrace();
-
-            }
         }
     }
 }
